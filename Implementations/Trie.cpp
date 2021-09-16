37de37350cb919bcc5e6b9f12f96b74c9fb2e9cb
Tiago Marinho <tiago.marinho.797@gmail.com>
2021-09-16 00:09:19 -0300

    Added Trie implementation

--- Implementations/Trie.cpp
+++ Implementations/Trie.cpp
@@ -1,8 +1,78 @@
 #include <bits/stdc++.h>
+#define ALPHABET_SIZE 26
 
 using namespace std;
 
-int main () {
+/**
+ * TRIE IMPLEMENTATION (Prefix Tree)
+ * 
+ * Insert
+ * Every char of the input key is inserted as an individual Trie Node.
+ * 
+ * Insert and search costs O(key_length)
+ * The Memory Requirement is O(ALPHABET_SIZE * key_length * N)
+ * Where N is the number of keys in Trie
+ */ 
+
+struct TrieNode {
+  struct TrieNode *children[ALPHABET_SIZE];
+  bool endOfWord;
+};
+
+struct TrieNode *createNode() {
+  TrieNode *newNode = new TrieNode;
+
+  newNode->endOfWord = false;
+
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    newNode->children[i] = NULL;
+  }
+
+  return newNode;
+}
+
+void insert(TrieNode *root, string key) {
+  TrieNode *auxNode = root;
+
+  for (int i = 0; i < key.size(); i++) {
+    int index = key[i] - 'a';
+    if(!auxNode->children[index]) {
+      auxNode->children[index] = createNode();
+    }
+
+    auxNode = auxNode->children[index];
+  }
+
+  auxNode->endOfWord = true;
+}
+
+bool search(TrieNode *root, string key) {
+  TrieNode *auxNode = root;
+
+  for (int i = 0; i < key.size(); i++) {
+    int index = key[i] - 'a';
+    if(!auxNode->children[index]) {
+      return false;
+    }
+
+    auxNode = auxNode->children[index];
+  }
+
+  return (auxNode->endOfWord);
+}
+
+int main() {
+
+  vector<string> keys = {"the", "a", "there","answer", "any", "by","bye", "their" };
+
+  int n = keys.size();
+  struct TrieNode *root = getNode();
+  //Build Trie
+  for (int i = 0; i < keys.size(); i++) {
+    insert(root, keys[i]);
+  }
+
+  cout << search(root, "the") << endl;
 
   return 0;
 }
--- Problems/sol.cpp
+++ Problems/sol.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
+#define ALPHABET_SIZE 26
 
 using namespace std;
 
+
 int main() {
+
 
   return 0;
 }
