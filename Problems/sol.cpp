37aa4ef8b50188965c09dd1b23e678c279fbab76
Tiago Marinho <tiago.marinho.797@gmail.com>
2021-09-22 23:50:22 -0300

    Solution k equal sum

--- Problems/sol.cpp
+++ Problems/sol.cpp
@@ -2,25 +2,36 @@
 
 using namespace std;
 
-class Solution {    
-public:
-    map<int, vector<int> > mp;
+//https://leetcode.com/problems/subarray-sum-equals-k/submissions/
 
-    Solution(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++) {
-          mp[nums[i]].push_back(i);
-        }
-    }
-    
-    int pick(int target) {
+int solution(vector<int> nums, int k) {
 
-     return 0;   
-    }
-};
+  int ans = 0, sum = 0;
+  map<int, int> prevSum;
+
+  prevSum[0] = 1;
+  for(auto i : nums) {
+    sum += i;
+
+    // if(sum == k) ans++;
+
+    // if(prevSum.find(sum - k) != prevSum.end()) {
+      ans += (prevSum[sum - k]);
+    // }
+
+    prevSum[sum - k]++;
+    prevSum[sum]++;
+  }
+
+
+  return ans;
+
+}
 
 int main() {
-  
 
+  vector<int> nums = {1, 1, 1};
+  cout << solution(nums, 2) << endl;
 
   return 0;
 }
