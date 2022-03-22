dce175f221fad9d44952d31d03a67936af11b7af
Tiago Marinho <tiago.marinho.797@gmail.com>
2022-03-22 20:33:11 -0300

    Added mutation for graphql project

--- React/GraphQL/graphql-pedrotech-react/src/DisplayData.tsx
+++ React/GraphQL/graphql-pedrotech-react/src/DisplayData.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useQuery, gql, useLazyQuery } from "@apollo/client"
+import { useQuery, gql, useLazyQuery, useMutation } from "@apollo/client"
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -20,6 +20,7 @@ const GET_MOVIE_BY_NAME = gql`
     }
   }
 `
+
 interface User {
   id: number;
   name: string;
@@ -27,10 +28,33 @@ interface User {
   username: string;
 }
 
+const CREATE_USER_MUTATION = gql`
+    mutation CreateUser($input: CreateUserInput!) {
+      createUser(input: $input) {
+        name,
+        id
+      }
+      
+    }
+  `
+
+/**
+ * Check Mutation and Queries
+ */
+
 const DisplayData = () => {
   const [movieSearched, setMovieSearched] = useState("")
+
+  //Create User Form
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
+  const [nationality, setNationality] = useState("")
+
   const { data, loading, error } = useQuery(QUERY_ALL_USERS);
   const [fetchMovie, { data: movieSearchedData, error: movieError }] = useLazyQuery(GET_MOVIE_BY_NAME)
+
+  const [createUser] = useMutation(CREATE_USER_MUTATION)
 
   if (loading) {
     return <h1>DATA IS LOADING....</h1>
@@ -49,7 +73,21 @@ const DisplayData = () => {
   }
 
   return (
-    <div>
+    <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+      <div>
+        <input onChange={(evt) => { setName(evt.target.value) }} type="text" placeholder="Name..." />
+        <input onChange={(evt) => { setUsername(evt.target.value) }} type="text" placeholder="Username..." />
+        <input onChange={(evt) => { setAge(Number(evt.target.value)) }} type="number" placeholder="Age..." />
+        <input onChange={(evt) => { setNationality(evt.target.value.toUpperCase()) }} type="text" placeholder="Nationality..." />
+        <button onClick={() => {createUser({variables: {
+          input: {
+            name,
+            username,
+            age,
+            nationality
+          }
+        }})}}>Create User</button>
+      </div>
       {data && data.users.map((user: User) => {
         return (
           <div key={user.id}>
@@ -63,9 +101,11 @@ const DisplayData = () => {
           placeholder="Interstellar..."
           onChange={(evt) => { setMovieSearched(evt.target.value) }}
         />
-        <button onClick={() => fetchMovie({variables: {
-          name: movieSearched
-        }})}>Fetch Data</button>
+        <button onClick={() => fetchMovie({
+          variables: {
+            name: movieSearched
+          }
+        })}>Fetch Data</button>
         <div>
           {movieSearchedData && <div>{`MovieName: ${movieSearchedData.movie.name}`}</div>}
         </div>
